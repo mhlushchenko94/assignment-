f65f45dba28742b28da6801a4afe5210049fbebc
Maksym Hlushchenko <mhlushchenko@HQ-MC-MHLUSH.local>
2020-01-19 15:57:54 -0500

    implemented test case

--- src/test/java/pageobject/LoginPO.java
+++ src/test/java/pageobject/LoginPO.java
@@ -1,6 +1,7 @@
 package pageobject;
 
 import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.testng.Assert;
@@ -18,13 +19,19 @@ public class LoginPO extends BasePO {
     }
 //
 
-    @FindBy(className = "linkNav")
-    WebElement linkNavigate;
+    @FindBy(className = "pipeWrap-sm")
+    WebElement menuBtn;
+
+    //    @FindBy(className = "linkNav")
+//    @FindBy(xpath = "//*[@id=\"header\"]/section[3]/div/nav/div/div/div/div/div[1]/div/ul/li[2]/div/a")
+    @FindBy(xpath = "//*[text()='Explore']")
+    WebElement linkNavigator;
 
     @FindBy(xpath = "//*[text()='Shore Excursions']")
     WebElement shoreExcursions;
 
-    @FindBy(className = "chosen-single")
+    @FindBy(xpath = "//*[@id='search_destinations']")
+//    @FindBy(name = "destinations")
     WebElement dropDown;
 
     @FindBy(xpath = "//*[text()='Alaska Cruises']")
@@ -33,15 +40,23 @@ public class LoginPO extends BasePO {
     @FindBy(xpath = "//*[text()='FIND EXCURSIONS']")
     WebElement findBtn;
 
+    @FindBy(id = "showLeftPush")
+    WebElement filter;
+
     @FindBy(xpath = "//*[@title='Port']")
     WebElement portFilter;
 
     @FindBy(xpath = "//*[@for='ports_VIC']")
     WebElement portVictoria;
 
+    @FindBy(xpath = "//*[@id=\"sap-menu-left\"]/div/div[4]/ul[1]/li[2]/div/div/div[1]/ul/li[3]/a")
+    WebElement doneBtn1;
 
+    @FindBy(xpath = "//*[@title='Update Filter']")
+    WebElement doneBtn2;
 
-
+    @FindBy(xpath = "//*[@id=\"shorexExplore\"]/div[1]/section/div/div/div[1]/ul/li[1]/figure/figcaption/div[2]/ul/li[2]/h2/a")
+    WebElement assertionByImage;
 
     public void loginTestPO() throws IOException, InterruptedException {
 
@@ -49,18 +64,19 @@ public class LoginPO extends BasePO {
 
         driver.get(propertyUtils.prop.getProperty("assignment.url"));
 
-        linkNavigate.click();
+        menuBtn.click();
+        Thread.sleep(5000);
+        linkNavigator.click();
         shoreExcursions.click();
         dropDown.click();
         alaskaCruises.click();
         findBtn.click();
+        filter.click();
         portFilter.click();
         portVictoria.click();
-
-
-
-
-
+        doneBtn1.click();
+        doneBtn2.click();
+        assertionByImage.isDisplayed();
 
 
     }
